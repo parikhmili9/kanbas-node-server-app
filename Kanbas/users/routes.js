078d784fbef613b3a5ef4b040ac6335589205a07
parikhmili9 <parikh.mi@northeastern.edu>
2024-04-17 15:29:59 -0400

    Find user by role feauture added

--- Kanbas/users/routes.js
+++ Kanbas/users/routes.js
@@ -17,8 +17,15 @@ export default function UserRoutes(app) {
     };
 
     const findAllUsers = async (req, res) => {
+        const {role} = req.query;
+        if(role){
+            const users = await dao.findUsersByRole(role);
+            res.json(users);
+            return;
+        }
         const users = await dao.findAllUsers();
         res.json(users);
+        return;
     };
     
     const findUserById = async (req, res) => {
