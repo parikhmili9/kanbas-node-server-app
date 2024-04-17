4605b7e6db306660e59fa59c5361574efa57028b
parikhmili9 <parikh.mi@northeastern.edu>
2024-04-17 10:01:26 -0400

    Completed upto section 4

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import express from 'express';
 // import Hello from "./Hello.js"
-import mongoose from "mongoose";
 import Lab5 from './Lab5.js';
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from './Kanbas/modules/routes.js';
 import AssignmentRoutes from './Kanbas/assignments/routes.js';
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from './Kanbas/users/routes.js';
 
 const app = express();
 app.use(express.json());
@@ -17,4 +18,5 @@ mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
